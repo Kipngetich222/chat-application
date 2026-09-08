5ac3d629e67761d347d3880b5083105a036f784f
Victor Yegon <victoryegon222@gmail.com>
2026-09-08 16:15:06 +0300

    a bit of code refactoring to match best practice on signup

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
+        const {MONGODB_URI} = process.env;
+        if (!MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined in the environment variables");
+        }
         const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log("MONGODB CONNECTED:", conn.connection.host);
         
