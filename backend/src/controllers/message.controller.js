270b1be90eef2954560fdeb1f0036cece875e214
Victor Yegon <victoryegon222@gmail.com>
2026-09-09 16:44:15 +0300

    did some code refactoring for messsage routes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -38,6 +38,18 @@ export const sendMessage = async (req, res) => {
         const {id: receiverId} = req.params;
         const senderId = req.user._id;
 
+    if (!text && !image) {
+        return res.status(400).json({ message: "Message text or image is required" });
+    }
+    if (senderId.toString() === receiverId.toString()) {
+        return res.status(400).json({ message: "You cannot send a message to yourself" });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+
+    if (!receiverExists) {
+        return res.status(404).json({ message: "Receiver not found" });
+    }
+
     let imageUrl;
     if (image) {
         // upload base64 image to cloudinary
