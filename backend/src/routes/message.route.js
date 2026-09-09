aec37b726f3811f1ffe2a047987227c0780aea52
Victor Yegon <victoryegon222@gmail.com>
2026-09-09 16:32:53 +0300

    implimentent messaging rautes for the app

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,18 @@
 import express from "express";
+import { getAllContacts, getMessagesByUserId, sendMessage, getchatPartners } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint!");
-});
+router.use(arcjetProtection, protectRoute); // Apply authentication protection to all routes in this router
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getchatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+
 
 
 export default router;
